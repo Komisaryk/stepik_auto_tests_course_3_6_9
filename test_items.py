44a8fdfc315d7666a45fb965ffd940d7649a7352
Komisaryk <komisaryky@gmail.com>
2022-06-02 03:44:21 +0300

    Adding files for Peer Review of 3_6_9

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element_by_class_name("btn-add-to-basket")
+    assert button,'no button'
